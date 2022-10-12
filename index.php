6cb27eb5b218d89ae8df1df4103dcfa5fead9d3f
zrottman <zrottman@example.com>
2022-10-12 13:51:33 -0700

    Add favicon

--- index.php
+++ index.php
@@ -36,9 +36,16 @@ $r_stats = @mysqli_query ($dbc, $q_stats);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link href='./resources/css/reset.css' rel='stylesheet'>
     <link href='./resources/css/style.css' rel='stylesheet'>
     <script type="text/javascript" src="./resources/js/scripts.js"></script>
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./favicon/favicon-16x16.png">
+    <link rel="manifest" href="./favicon/site.webmanifest">
+
     <title>He's sick!</title>
   </head>
 
